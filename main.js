eab9eee79975bee27d76e3a613df145b0684ca41
Paolo-dev-it <paolo.nicoletti99@gmail.com>
2022-11-10 14:08:42 +0100

    Created message and added image taken from data. Project completed!

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
+/* Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data. */
+
 var app = new Vue({
   el: '#root',
   data: {
-    message: 'Hello Vue!'
+    message: 'Ciao Boolean!',
+    image: './assets/img/pexels-chevanon-photography-1108099.jpg',
+    classeDinamica: 'dimension'
   },
   methods: {
-    
+
   }
 })
